6bd1c22630afd7aca4f226c1fb5920ae1020fcf0
AramKocharyan <aram.koch@gmail.com>
2016-11-19 11:04:43 +0400

    Add project files.

---
+++ EntityFramework.Guardian.Entities/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("EntityFramework.Guardian.Entities")]
+[assembly: AssemblyDescription("EntityFramework.Guardian separate package for entities")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Aram Kocharyan")]
+[assembly: AssemblyProduct("EntityFramework.Guardian.Entities")]
+[assembly: AssemblyCopyright("Copyright \u00A9 Aram Kocharyan 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("63b2b287-edb3-4d9a-a02a-a6e2f1523b97")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.3.5.0")]
