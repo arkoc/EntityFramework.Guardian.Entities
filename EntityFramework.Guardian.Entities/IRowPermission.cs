e323ff8bb1effae7cc6b108e360810fb203e6dd0
AramKocharyan <aram.koch@gmail.com>
2016-11-22 16:40:29 +0400

    removed dublicate code

--- EntityFramework.Guardian.Entities/IRowPermission.cs
+++ EntityFramework.Guardian.Entities/IRowPermission.cs
@@ -6,16 +6,8 @@ namespace EntityFramework.Guardian.Entities
     /// <summary>
     /// Entity interfce presenting permission linked to entitytype and row identifier
     /// </summary>
-    public interface IRowPermission
+    public interface IRowPermission : IPermission
     {
-        /// <summary>
-        /// Gets the name of the entity type.
-        /// </summary>
-        /// <value>
-        /// The name of the entity type.
-        /// </value>
-        string EntityTypeName { get; }
-
         /// <summary>
         /// Gets the row identifier.
         /// </summary>
@@ -23,13 +15,5 @@ namespace EntityFramework.Guardian.Entities
         /// The row identifier.
         /// </value>
         string RowIdentifier { get; }
-
-        /// <summary>
-        /// Gets the type of the access.
-        /// </summary>
-        /// <value>
-        /// The type of the access.
-        /// </value>
-        AccessTypes AccessType { get; }
     }
 }
